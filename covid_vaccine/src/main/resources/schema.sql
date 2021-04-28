07f57158329bab5d2d69662f895f22f6078b908a
christian zuna <christian.zunalargo@revature.net>
2021-04-28 17:50:35 -0400

    schema.sql file

--- covid_vaccine/src/main/resources/schema.sql
+++ covid_vaccine/src/main/resources/schema.sql
@@ -1,24 +1,26 @@
 CREATE TABLE patients ( 
-    id serial primary key, 
+    patients_id serial primary key, 
     fname text not null,
     lname text not null,
     age integer not null,
     sex text not null,
-    username text not null,
+    username text not null, 
     pass text not null
 );
 
 CREATE TABLE vaccineDistributor (
-    id serial primary key,
+    distributor_id serial primary key,
     distributorName text not null,
-    location_address text not null, 
+    pass text not null,
+    location_address text not null,  
     city text not null, 
     countryState text not null,
     zipcode integer not null
  );
 
 CREATE TABLE appointments (
-    id serial primary key, 
-    patient_id integer references patients (id),
-    vaccineDistributor_id integer references vaccineDistributor(id)
+    appt_id serial primary key, 
+    patient_id integer references patients(patients_id),
+    vaccineDistributor_id integer references vaccineDistributor(distributor_id,
+    appt_date date not null
  );
