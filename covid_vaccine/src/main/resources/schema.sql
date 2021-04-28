c3cf75c8144ac08d04ec7d5a3ff8d2d5c2f0aa8b
christian zuna <christian.zunalargo@revature.net>
2021-04-28 17:36:30 -0400

    squema

--- covid_vaccine/src/main/resources/schema.sql
+++ covid_vaccine/src/main/resources/schema.sql
@@ -2,24 +2,19 @@ CREATE TABLE patients (
     id serial primary key, 
     fname text not null,
     lname text not null,
-    age text not null,
+    age integer not null,
     sex text not null,
     username text not null,
     pass text not null
 );
 
-CREATE TABLE locations (
-    id serial primary key,
-    streetNum integer not null, 
-    city text not null, 
-    countryState text not null,
-    zipcode integer not null
- );
-
 CREATE TABLE vaccineDistributor (
     id serial primary key,
     distributorName text not null,
-    locations_id integer references locations(id)
+    location_address text not null, 
+    city text not null, 
+    countryState text not null,
+    zipcode integer not null
  );
 
 CREATE TABLE appointments (
